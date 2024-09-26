fb7fea71cc7f57314fff1de882463a9664613abe
John Arrandale <bootsie227@gmail.com>
2024-09-26 11:12:10 -0400

    update(file): - Added default logging regardless of environment - Added production context to Docker publish pipeline

--- src/lib/Logger.ts
+++ src/lib/Logger.ts
@@ -2,8 +2,6 @@ import { createLogger, format, transports } from "winston";
 import { setGlobalConfig } from "axios-logger";
 import stringify from "fast-safe-stringify";
 
-import environment from "../environment";
-
 const logger = createLogger({
   level: "info",
   format: format.json(),
@@ -14,36 +12,29 @@ const logger = createLogger({
 });
 
 setGlobalConfig({
-  data: false,
-  dateFormat: "isoDateTime"
+  headers: true,
+  params: true,
+  data: true
 });
 
-if (!environment.production) {
-  setGlobalConfig({
-    headers: true,
-    params: true,
-    data: true
-  });
+logger.add(
+  new transports.Console({
+    level: "debug",
+    format: format.combine(
+      format.timestamp(),
+      format.simple(),
+      format.colorize(),
+      format.printf(options => {
+        const args = options[Symbol.for("splat")]?.filter(
+          (arg: any) => !(arg instanceof Error)
+        );
 
-  logger.add(
-    new transports.Console({
-      level: "debug",
-      format: format.combine(
-        format.timestamp(),
-        format.simple(),
-        format.colorize(),
-        format.printf(options => {
-          const args = options[Symbol.for("splat")]?.filter(
-            (arg: any) => !(arg instanceof Error)
-          );
+        const argsString = args?.map(stringify).join(" ");
 
-          const argsString = args?.map(stringify).join(" ");
-
-          return `${options.timestamp} ${options.level} [${options.service}]${options.label ? " [" + options.label + "]" : ""} ${options.message}${argsString ? " " + argsString : ""}${options.stack ? "\n" + options.stack : ""}`;
-        })
-      )
-    })
-  );
-}
+        return `${options.timestamp} ${options.level} [${options.service}]${options.label ? " [" + options.label + "]" : ""} ${options.message}${argsString ? " " + argsString : ""}${options.stack ? "\n" + options.stack : ""}`;
+      })
+    )
+  })
+);
 
 export default logger;
